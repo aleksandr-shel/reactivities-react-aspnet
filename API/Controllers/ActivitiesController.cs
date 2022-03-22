b706b076b51ae2b8fbb451e4114e3abd2ace3577
Aleksandr Shelukheev <shelukheev@gmail.com>
2022-03-21 21:33:31 -0400

    added CRUD on backend using mediator
    CQRS pattern

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,8 @@
 
 
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -9,20 +11,34 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities(){
-            return await _context.Activities.ToListAsync();
+        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken ct)
+        {
+            return await Mediator.Send(new List.Query(), ct);
         }
 
         [HttpGet("{id}")] //activities/id
-        public async Task<ActionResult<Activity>> GetActivity(Guid id){
-            return await _context.Activities.FindAsync(id);
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity){
+            return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activtiy){
+            activtiy.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activtiy}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id){
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
         }
     }
 }
