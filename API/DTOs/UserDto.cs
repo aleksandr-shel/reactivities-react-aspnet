fbcc08ce9fe563e71d08ec20ff28f65c9e8931c9
Aleksandr Shelukheev <shelukheev@gmail.com>
2022-05-13 17:59:32 -0400

    added authentication

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.DTOs
+{
+    //DTO-Data transfer object
+    //what information passes to user when logged in
+    public class UserDto
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string Image { get; set; }
+    }
+}
