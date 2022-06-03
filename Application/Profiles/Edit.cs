eca0b047d9ba0fe9d15738366a3e109ed46678e9
Aleksandr Shelukheev <shelukheev@gmail.com>
2022-06-03 19:10:53 -0400

    added updating profile feature

---
+++ Application/Profiles/Edit.cs
@@ -0,0 +1,57 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class Edit
+    {
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string Bio { get; set; }
+            public string DisplayName { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(c => c.DisplayName).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _dataContext;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext dataContext, IUserAccessor userAccessor)
+            {
+                _dataContext = dataContext;
+                _userAccessor = userAccessor;
+            }
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.UserName == _userAccessor.GetUsername());
+
+                if (user == null) return null;
+
+                user.DisplayName = request.DisplayName ?? user.DisplayName;
+                user.Bio = request.Bio ?? user.Bio;
+
+                var result = await _dataContext.SaveChangesAsync() > 0;
+                if (!result) return Result<Unit>.Failure("there were no changes to save to the Database");
+
+                return Result<Unit>.Success(Unit.Value);
+            }
+        }
+    }
+}
