d6b442ed22eb02c90884337d265052c41c06d0af
Aleksandr Shelukheev <shelukheev@gmail.com>
2022-06-18 14:05:46 -0400

    security headers added

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,11 +42,33 @@ namespace API
             //our own exception middleware
             app.UseMiddleware<ExceptionMiddleware>();
 
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opt => opt.NoReferrer());
+            app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+            app.UseXfo(opt=>opt.Deny());
+            app.UseCsp(opt=>opt
+                .BlockAllMixedContent()
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "https://cdnjs.cloudflare.com"))
+                .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:", "https://cdnjs.cloudflare.com"))
+                .FormActions(s =>s.Self())
+                .FrameAncestors(s => s.Self())
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ScriptSources(s => s.Self().CustomSources("sha256-kPx0AsF0oz2kKiZ875xSvv693TBHkQ/0SkMJZnnNpnQ=", "http://conoret.com"))
+                );
+
             if (env.IsDevelopment())
             {
                 //app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+            } else
+            {
+                //app.UseHsts();
+                app.Use(async (context, next) =>
+                {
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=3156000");
+                    await next.Invoke();
+                });
             }
 
             // app.UseHttpsRedirection();
